5657dabf198b939e9212ca91e4b650c755e2b5ad
seongsuhwan <hansim00315@gmail.com>
2024-07-28 23:01:49 +0900

    commit

---
+++ src/main/java/com/example/doodelivery/DooDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.doodelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DooDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DooDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/doodelivery/DooDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doodelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DooDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
